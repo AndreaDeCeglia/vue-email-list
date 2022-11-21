081e4c71a925373a3e8226ca612da032ee88249a
AndreaDeCeglia <deceglia89@gmail.com>
2022-11-21 16:45:59 +0100

    structure done

---
+++ script.js
@@ -0,0 +1,20 @@
+var app = new vue({
+    el : '#root',
+    data: {
+
+    },
+
+    mounted() {
+
+    },
+    created() {
+  
+    },
+    beforeUpdate() {
+  
+    },
+    
+    methods: {
+        
+    }
+})
